02cbc94aa78876d3c82fd7691efb1a20250397e4
caorpr <cand3952@740s-pl-115.local>
2016-03-23 12:40:13 -0600

    The runner, and basics of the game!

---
+++ src/game/controller/GameRunner.java
@@ -0,0 +1,11 @@
+package game.controller;
+
+public class GameRunner 
+{
+
+	public static void main (String [] args)
+	{
+		GameRunner myGameController = new GameRunner();
+		myGameController.start();
+	}
+}
